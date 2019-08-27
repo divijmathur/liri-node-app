1bd3e20d0a65e313004a4bd9ea833b38a7ce32d1
Mona Lisa <divijmathur@gmail.com>
2019-08-27 12:38:23 -0700

    added spotify search feature and keys

--- liri.js
+++ liri.js
@@ -1,21 +1,11 @@
-// var userInput = process.argv;
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify ({
-//     id: "ae69391e6794483589176782528ca45f",
-//     secret: "1ac1a1df5c864959bcbf2eadbfd59e03"
-// });
-// spotify.search({
-//     type: "track",
-//     query: "All the Small Things"
-// }, function(err, data){
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(data);
-// });
-
+require("dotenv").config();
+var keys = require("./keys.js")
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 const axios = require('axios');
 var fs = require('fs');
+var moment = require('moment');
+moment().format();
 var command = process.argv[2];
 var value = process.argv[3];
 switch (command) {
@@ -25,6 +15,14 @@ switch (command) {
     
     case "concert-this":
         concertThis(value);
+        break;
+
+    case "do-what-it-says":
+        doWhat(value);
+        break;
+    
+    case "spotify-this-song":
+        spotifyThis(value);
         break;
 }
 
@@ -43,5 +41,39 @@ function movieThis(value) {
 }
 // build api call for bands in town after receiving personal app id from them
 function concertThis(value) {
-    axios.get("https://rest.bandsintown.com/event/13722599?app_id=foo&artist=" + value + "&came_from=67")
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events&app_id=6d566b33d3c4271daf35c85ea1d7a9f9")
+    .then(function(response){
+        // console.log(response.data);
+        for(var i = 0; i < response.data.length; i++) {
+            var datetime = response.data[i].datetime;
+            var dateArr = datetime.split("T");
+            var concertResults = "\nVenue Name " + response.data[i].venue.name + "\nVenue Location " + response.data[i].venue.city + "\nDate of the event " + moment(dateArr[0], "MM-DD-YYYY");
+            console.log(concertResults);
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
+
+function doWhat(value) {
+    fs.readFile('random.txt','utf8', function(err,data) {
+        if (error) {
+            console.log(error);
+        }
+        var dataArr = data.split(',');
+        spotifySong(dataArr[0], dataArr[1]);
+    })
+}
+
+function spotifyThis(value) {
+    spotify.search({ type: 'track', query: value, limit: 1}, function(err,data) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song name: " + data.tracks.items[0].name);
+        console.log("Preview on Spotify " + data.tracks.items[0].uri);
+        console.log("Album " + data.tracks.items[0].album.name);
+    });
 }
